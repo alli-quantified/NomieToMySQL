064ebbc81c961cbcdfeada6ff8fd9f96bf4fd248
Alli <none@none.com>
2017-09-30 18:57:04 -0600

    Tidy up couch call for local connection.

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ from funcs import *
 conn = mysql.connector.connect(user = 'DBUsername', password = 'DBPassword', host = 'localhost', database = 'nomie')
 c = conn.cursor()
 
-couch = couchdb.Server('CouchDBServer')
+# Examples..
+# couch = couchdb.Server('http://example.com:5984/')
+# couch = couchdb.Server('https://username:password@host:port/')
 
+couch = couchdb.Server()
 
-t = couch['nomieuser_trackers']
-e = couch['nomieuser_events']
+t = couch['nomie_trackers']
+e = couch['nomie_events']
 
 if __name__ == "__main__":
 	if len(sys.argv) == 1:	## Default way to run the script
